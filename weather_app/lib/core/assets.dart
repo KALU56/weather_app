5f532f0ebb363c231ec727eb0534dbe8edfea146
kalkidanbehailu55@gmail.com <kalkidanbehailu55@gmail.com>
2025-07-29 10:23:40 +0300

    feat: finish onbording screen

--- weather_app/lib/core/assets.dart
+++ weather_app/lib/core/assets.dart
@@ -2,9 +2,9 @@ class Assets {
   Assets._();
   // static const tag = 'assets/images/tags.png';
   static const weather = 'assets/image/weater1.png';
-  // static const image2 = 'assets/images/image1.png';
-  // static const image3 = 'assets/images/image2.png';
-  //  static const image4 = 'assets/images/image3.png';
+  static const image2 = 'assets/image/weather1.png';
+  static const image3 = 'assets/image/weather3.png';
+   static const image4 = 'assets/image/weather4.png';
   // static const image5 = 'assets/images/User research-pana.png';
   // static const appleImage = 'assets/images/apple.png';
   // static const googleImage = 'assets/images/google.png';
--- weather_app/lib/screens/onbording.dart
+++ weather_app/lib/screens/onbording.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/core/assets.dart';
+import 'package:weather_app/screens/home.dart';
 
 class Onbording extends StatefulWidget {
   const Onbording({super.key});
@@ -8,8 +10,186 @@ class Onbording extends StatefulWidget {
 }
 
 class _OnbordingState extends State<Onbording> {
+  final PageController _pageController = PageController();
+  int _currentIndex = 0;
+
+  final List<Map<String, String>> onboardingData = [
+    {
+      'title': 'Team Up For Success',
+      'desc':
+          'Team Up For Success Team Up For Success Team Up For Success Team Up For Success Team Up For Success ',
+      'image': Assets.image2,
+    },
+    {
+      'title': 'User-Friendly at its Core',
+      'desc':
+          'Team Up For Success Team Up For Success Team Up For Success Team Up For Success vvTeam Up For Success .',
+      'image': Assets.image3,
+    },
+    {
+      'title': 'Easy Task Creation',
+      'desc':
+          'Team Up For Success Team Up For Success vvvTeam Up For Success Team Up For Success .',
+      'image': Assets.image4,
+    },
+  ];
+
+  void _nextPage() {
+    if (_currentIndex < onboardingData.length - 1) {
+      _pageController.animateToPage(
+        _currentIndex + 1,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const Home()),
+      );
+    }
+  }
+
+  void _skip() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const Home()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 24),
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(Assets.weather),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 15),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Home()),
+                  );
+                },
+                child: const Text(
+                  'Weather',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900,
+                    color:  Color.fromARGB(255, 99, 203, 245),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          Expanded(
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: onboardingData.length,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              itemBuilder: (context, index) {
+                final item = onboardingData[index];
+                return Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Image.asset(item['image']!, height: 300)],
+                  ),
+                );
+              },
+            ),
+          ),
+
+          const SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              onboardingData.length,
+              (index) => AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                margin: const EdgeInsets.symmetric(horizontal: 6),
+                width: _currentIndex == index ? 20 : 8,
+                height: 8,
+                decoration: BoxDecoration(
+                  color: _currentIndex == index
+                      ? const Color.fromARGB(255, 99, 203, 245)
+                      : const Color.fromARGB(255, 128, 200, 241),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+            ),
+          ),
+
+            
+           Column(
+            children: [
+              Text(
+                onboardingData[_currentIndex]['title']!,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 12),
+              Text(
+                onboardingData[_currentIndex]['desc']!,
+                style: const TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        
+
+          const SizedBox(height: 24),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 80, 224, 250)
+                // minimumSize: const Size(double.infinity, 50),
+              ),
+              onPressed: _nextPage,
+              child: Text(
+                _currentIndex == onboardingData.length - 1 ? 'Home' : 'Next',
+              ),
+            ),
+          ),
+
+          TextButton(
+            onPressed: _skip,
+
+            child: Text(
+              _currentIndex == onboardingData.length - 1 ? 'Home' : 'Skip',
+              style: TextStyle(color: const Color.fromARGB(255, 117, 226, 245),
+            ),
+          ),
+        
+          ),
+            SizedBox(height: 16),
+
+        ],
+      ),
+    );
   }
 }
