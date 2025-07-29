5f6e35ed801a7cf39cb6c18f4fcd5cceb770c23e
kalkidanbehailu55@gmail.com <kalkidanbehailu55@gmail.com>
2025-07-29 11:04:39 +0300

    feat: add white color search bar and also modfy the background color

--- weather_app/lib/screens/home.dart
+++ weather_app/lib/screens/home.dart
@@ -10,6 +10,40 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 89, 148, 250),
+      body: Padding(
+      
+        padding: const EdgeInsets.all(8.0),
+        
+        child: Column(
+          children: [
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Search here...',
+                hintStyle: const TextStyle(color: Colors.white),
+                prefixIcon: const Icon(Icons.search,color: Colors.white),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(35),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(35),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(35),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.2),
+              ),
+            
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
