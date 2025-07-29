0a64f615e30bf7b30755025bc2947fda71363df2
kalkidanbehailu55@gmail.com <kalkidanbehailu55@gmail.com>
2025-07-29 12:47:55 +0300

    feat: add the hourly forecast

---
+++ weather_app/lib/screens/a.dart
@@ -0,0 +1,262 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final TextEditingController _controller = TextEditingController();
+  Map<String, dynamic>? weatherData;
+  bool isLoading = false;
+  String? errorMessage;
+
+
+  final String apiKey = '';
+
+  Future<void> fetchWeather(String city) async {
+  setState(() {
+    isLoading = true;
+    errorMessage = null;
+    weatherData = null;
+    hourlyForecast = null;
+  });
+
+    final url = Uri.parse(
+      'https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$apiKey',
+    );
+     final forecastUrl = Uri.parse(
+    'https://api.openweathermap.org/data/2.5/forecast?q=$city&units=metric&appid=$apiKey',
+  );
+
+
+
+  try {
+    final currentResponse = await http.get(currentUrl);
+    final forecastResponse = await http.get(forecastUrl);
+
+    if (currentResponse.statusCode == 200 && forecastResponse.statusCode == 200) {
+      final currentData = json.decode(currentResponse.body);
+      final forecastData = json.decode(forecastResponse.body);
+
+      setState(() {
+        weatherData = currentData;
+        hourlyForecast = forecastData['list'].take(5).toList(); // only next 5 (next 15 hrs)
+      });
+    } else {
+      setState(() {
+        errorMessage = 'City not found. Please try again.';
+      });
+    }
+  } catch (e) {
+    setState(() {
+      errorMessage = 'Error fetching weather. Check your connection.';
+    });
+  } finally {
+    setState(() {
+      isLoading = false;
+    });
+  }
+}
+    void _search() {
+    final city = _controller.text.trim();
+    if (city.isNotEmpty) {
+      fetchWeather(city);
+      FocusScope.of(context).unfocus(); 
+    }
+  }
+    IconData getWeatherIcon(String mainWeather) {
+    switch (mainWeather.toLowerCase()) {
+      case 'clear':
+        return Icons.wb_sunny;
+      case 'clouds':
+        return Icons.cloud;
+      case 'rain':
+        return Icons.beach_access;
+      case 'snow':
+        return Icons.ac_unit;
+      case 'thunderstorm':
+        return Icons.flash_on;
+      case 'drizzle':
+        return Icons.grain;
+      case 'mist':
+      case 'fog':
+      case 'haze':
+      case 'smoke':
+      case 'dust':
+        return Icons.blur_on;
+      default:
+        return Icons.help_outline;
+    }
+  }
+    Widget buildWeatherInfo(IconData icon, String value, String label) {
+    return Column(
+      children: [
+        Icon(icon, color: const Color.fromARGB(255, 99, 203, 245)),
+        Text(value, style: const TextStyle(color: Colors.white)),
+        Text(label, style: const TextStyle(color: Colors.white)),
+      ],
+    );
+    
+  }
+ Widget buildHourlyItem(Map<String, dynamic> item) {
+  final temp = item['main']['temp'].round().toString();
+  final condition = item['weather'][0]['main'];
+  final time = item['dt_txt'];
+
+  final hour = DateTime.parse(time).hour;
+  final label = hour == DateTime.now().hour ? "Now" : "${hour % 12 == 0 ? 12 : hour % 12} ${hour >= 12 ? 'PM' : 'AM'}";
+
+  return Column(
+    children: [
+      Text(label, style: const TextStyle(color: Colors.white)),
+      Icon(
+        getWeatherIcon(condition),
+        color: const Color.fromARGB(255, 99, 203, 245),
+      ),
+      Text('$temp°C', style: const TextStyle(color: Colors.white)),
+    ],
+  );
+}
+    List<Widget> getWeatherDetailsWidgets(Map<String, dynamic> data) {
+    final main = data['weather'][0]['main'].toLowerCase();
+
+    if (main == 'rain' || main == 'drizzle' || main == 'thunderstorm') {
+      return [
+        buildWeatherInfo(Icons.beach_access, '${data['rain']?['1h'] ?? 0} mm', 'Rain'),
+        buildWeatherInfo(Icons.air, '${data['wind']['speed']} km/h', 'Wind'),
+        buildWeatherInfo(Icons.opacity, '${data['main']['humidity']}%', 'Humidity'),
+      ];
+    } else if (main == 'clear') {
+      return [
+        buildWeatherInfo(Icons.wb_sunny, 'High', 'UV'),
+        buildWeatherInfo(Icons.opacity, '${data['main']['humidity']}%', 'Humidity'),
+        buildWeatherInfo(Icons.air, '${data['wind']['speed']} km/h', 'Wind'),
+      ];
+    } else if (main == 'clouds') {
+      return [
+        buildWeatherInfo(Icons.cloud, 'Cloudy', 'Sky'),
+        buildWeatherInfo(Icons.opacity, '${data['main']['humidity']}%', 'Humidity'),
+        buildWeatherInfo(Icons.air, '${data['wind']['speed']} km/h', 'Wind'),
+      ];
+    } else {
+      return [
+        buildWeatherInfo(Icons.info, 'N/A', 'Info'),
+      ];
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 89, 148, 250),
+      body: Padding(
+      
+        padding: const EdgeInsets.all(8.0),
+        
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Search here...',
+                hintStyle: const TextStyle(color: Colors.white),
+                prefixIcon: const Icon(Icons.search,color: Colors.white),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(35),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(35),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(35),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.2),
+              ),
+             onSubmitted: (_) => _search(),
+            ),
+            const SizedBox(height: 45),
+             if (isLoading) 
+              const CircularProgressIndicator(color: Colors.white) 
+            else if (errorMessage != null) 
+              Text(errorMessage!, style: const TextStyle(color: Colors.red, fontSize: 18)) 
+            else if (weatherData != null) 
+
+            Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${weatherData!['name']}, ${weatherData!['sys']['country']}',
+                      style: const TextStyle(color: Colors.white, fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    Icon(
+                      getWeatherIcon(weatherData!['weather'][0]['main']),
+                      size: 70,
+                      color: const Color.fromARGB(255, 99, 203, 245),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      '${weatherData!['main']['temp'].round()}°C',
+                      style: const TextStyle(color: Colors.white, fontSize: 50),
+                    ),
+                    Text(
+                      weatherData!['weather'][0]['description'],
+                      style: const TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: getWeatherDetailsWidgets(weatherData!),
+                    ),
+                  ],
+                ),
+        if (hourlyForecast != null)
+  Column(
+    children: [
+      const Row(
+        children: [
+          Icon(Icons.access_time, color: Colors.white),
+          SizedBox(width: 8),
+          Text('Hourly Forecast', style: TextStyle(color: Colors.white, fontSize: 18)),
+        ],
+      ),
+      const SizedBox(height: 10),
+      SizedBox(
+        height: 100,
+        child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          itemCount: hourlyForecast!.length,
+          separatorBuilder: (_, __) => const SizedBox(width: 20),
+          itemBuilder: (context, index) {
+            return buildHourlyItem(hourlyForecast![index]);
+          },
+        ),
+      ),
+    ],
+  )
+            
+            else
+              const Expanded(
+                child: Center(
+                  child: Text(
+                    'Search for a city to get weather info',
+                    style: TextStyle(color: Colors.white70, fontSize: 18),
+                  ),
+                ),
+              ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
--- weather_app/lib/screens/home.dart
+++ weather_app/lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -11,54 +12,60 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final TextEditingController _controller = TextEditingController();
   Map<String, dynamic>? weatherData;
+  List<dynamic>? hourlyForecast;
+  String city = "Addis Ababa";
   bool isLoading = false;
   String? errorMessage;
 
+  final String apiKey = '56f255812ae46fcf7ae2f411cc5262c9'; // <-- Add your real OpenWeatherMap API key here
 
-  final String apiKey = '56f255812ae46fcf7ae2f411cc5262c9';
+  @override
+  void initState() {
+    super.initState();
+    fetchWeather(city);
+  }
 
   Future<void> fetchWeather(String city) async {
     setState(() {
       isLoading = true;
-      errorMessage = null;
-      weatherData = null;
+      errorMessage = null; // Reset error before new fetch
     });
 
-    final url = Uri.parse(
-      'https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$apiKey',
-    );
-
     try {
-      final response = await http.get(url);
+      final url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$apiKey',
+      );
+      final forecastUrl = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/forecast?q=$city&units=metric&appid=$apiKey',
+      );
 
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
+      final currentResponse = await http.get(url);
+      final forecastResponse = await http.get(forecastUrl);
+
+      if (currentResponse.statusCode == 200 && forecastResponse.statusCode == 200) {
         setState(() {
-          weatherData = data;
+          weatherData = json.decode(currentResponse.body);
+          hourlyForecast = json.decode(forecastResponse.body)['list'];
+          isLoading = false;
         });
       } else {
         setState(() {
-          errorMessage = 'City not found. Please try again.';
+          weatherData = null;
+          hourlyForecast = null;
+          isLoading = false;
+          errorMessage = "City not found or API error";
         });
       }
     } catch (e) {
       setState(() {
-        errorMessage = 'Error fetching weather. Check your connection.';
-      });
-    } finally {
-      setState(() {
         isLoading = false;
+        errorMessage = "Failed to fetch weather data";
       });
+      print("Error: $e");
     }
   }
-    void _search() {
-    final city = _controller.text.trim();
-    if (city.isNotEmpty) {
-      fetchWeather(city);
-      FocusScope.of(context).unfocus(); 
-    }
-  }
-    IconData getWeatherIcon(String mainWeather) {
+
+  IconData getWeatherIcon(String mainWeather) {
     switch (mainWeather.toLowerCase()) {
       case 'clear':
         return Icons.wb_sunny;
@@ -82,16 +89,27 @@ class _HomeState extends State<Home> {
         return Icons.help_outline;
     }
   }
-    Widget buildWeatherInfo(IconData icon, String value, String label) {
+
+  Widget buildWeatherIcon(String iconCode, {double size = 60}) {
+    return Image.network(
+      'https://openweathermap.org/img/wn/$iconCode@2x.png',
+      width: size,
+      height: size,
+    );
+  }
+
+  Widget buildWeatherInfo(IconData icon, String value, String label) {
     return Column(
       children: [
         Icon(icon, color: const Color.fromARGB(255, 99, 203, 245)),
+        const SizedBox(height: 4),
         Text(value, style: const TextStyle(color: Colors.white)),
         Text(label, style: const TextStyle(color: Colors.white)),
       ],
     );
   }
-    List<Widget> getWeatherDetailsWidgets(Map<String, dynamic> data) {
+
+  List<Widget> getWeatherDetailsWidgets(Map<String, dynamic> data) {
     final main = data['weather'][0]['main'].toLowerCase();
 
     if (main == 'rain' || main == 'drizzle' || main == 'thunderstorm') {
@@ -124,18 +142,17 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 89, 148, 250),
       body: Padding(
-      
-        padding: const EdgeInsets.all(8.0),
-        
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            // Search field
             TextField(
               controller: _controller,
               style: const TextStyle(color: Colors.white),
               decoration: InputDecoration(
-                hintText: 'Search here...',
+                hintText: 'Search city...',
                 hintStyle: const TextStyle(color: Colors.white),
-                prefixIcon: const Icon(Icons.search,color: Colors.white),
+                prefixIcon: const Icon(Icons.search, color: Colors.white),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(35),
                   borderSide: const BorderSide(color: Colors.white),
@@ -151,62 +168,123 @@ class _HomeState extends State<Home> {
                 filled: true,
                 fillColor: Colors.white.withOpacity(0.2),
               ),
-             onSubmitted: (_) => _search(),
+              onSubmitted: (value) {
+                city = value.trim();
+                if (city.isNotEmpty) {
+                  fetchWeather(city);
+                }
+              },
             ),
-            const SizedBox(height: 45),
-             if (isLoading) 
-              const CircularProgressIndicator(color: Colors.white) 
-            else if (errorMessage != null) 
-              Text(errorMessage!, style: const TextStyle(color: Colors.red, fontSize: 18)) 
-            else if (weatherData != null) 
+            const SizedBox(height: 20),
 
-            Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      '${weatherData!['name']}, ${weatherData!['sys']['country']}',
-                      style: const TextStyle(color: Colors.white, fontSize: 28, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 10),
-                    Icon(
-                      getWeatherIcon(weatherData!['weather'][0]['main']),
-                      size: 70,
-                      color: const Color.fromARGB(255, 99, 203, 245),
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      '${weatherData!['main']['temp'].round()}°C',
-                      style: const TextStyle(color: Colors.white, fontSize: 50),
-                    ),
-                    Text(
-                      weatherData!['weather'][0]['description'],
-                      style: const TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: getWeatherDetailsWidgets(weatherData!),
-                    ),
-                  ],
-                ),
-            Expanded(
-              child: Row(
-                te
-
-
+            // Show error message if any
+            if (errorMessage != null)
+              Text(
+                errorMessage!,
+                style: const TextStyle(color: Colors.redAccent, fontSize: 18),
               ),
-            )
-            
-            else
-              const Expanded(
-                child: Center(
-                  child: Text(
-                    'Search for a city to get weather info',
-                    style: TextStyle(color: Colors.white70, fontSize: 18),
+
+            // Show loading indicator while fetching
+            if (isLoading)
+              const Center(child: CircularProgressIndicator()),
+
+            // Show weather data when available and not loading
+            if (weatherData != null && !isLoading)
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${weatherData!['name']}, ${weatherData!['sys']['country']}',
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      Icon(
+                        getWeatherIcon(weatherData!['weather'][0]['main']),
+                        size: 70,
+                        color: const Color.fromARGB(255, 99, 203, 245),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        '${weatherData!['main']['temp'].round()}°C',
+                        style: const TextStyle(color: Colors.white, fontSize: 50),
+                      ),
+                      Text(
+                        weatherData!['weather'][0]['description'],
+                        style: const TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: getWeatherDetailsWidgets(weatherData!),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        'Hourly Forecast',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        height: 120,
+                        child: hourlyForecast != null
+                            ? ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount:
+                                    hourlyForecast!.length < 10 ? hourlyForecast!.length : 10,
+                                itemBuilder: (context, index) {
+                                  final hourData = hourlyForecast![index];
+                                  final time = DateTime.parse(hourData['dt_txt']);
+                                  final hour = '${time.hour}:00';
+                                  final icon = hourData['weather'][0]['icon'];
+                                  final temp = hourData['main']['temp'];
+
+                                  return Container(
+                                    margin:
+                                        const EdgeInsets.symmetric(horizontal: 8),
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white.withOpacity(0.2),
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(hour,
+                                            style: const TextStyle(
+                                                color: Colors.white)),
+                                        buildWeatherIcon(icon, size: 40),
+                                        Text('${temp.toStringAsFixed(1)}°C',
+                                            style: const TextStyle(
+                                                color: Colors.white)),
+                                      ],
+                                    ),
+                                  );
+                                },
+                              )
+                            : const Center(
+                                child: Text('Loading...',
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                      )
+                    ],
                   ),
                 ),
               ),
-            
+
+            // Prompt to enter a city if nothing else to show
+            if (weatherData == null && !isLoading && errorMessage == null)
+              const Center(
+                child: Text('Enter a city',
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+              ),
           ],
         ),
       ),
